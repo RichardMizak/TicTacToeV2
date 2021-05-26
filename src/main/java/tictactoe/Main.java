b47510109cadeac3110c4fbfc5e73e2a092234bb
RichardMizak <richard.mizak@kosickaakademia.sk>
2021-05-26 10:37:26 +0200

    adde winner

--- src/main/java/tictactoe/Main.java
+++ src/main/java/tictactoe/Main.java
@@ -1,16 +1,18 @@
 package tictactoe;
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         main.printMatrix();
-        main.scanRows();
+        switch (main.isWinner()){
+            case 1:
+                System.out.println("Player 1 wins.");
+            case 2:
+                System.out.println("Player 2 wins.");
+
+        }
+
     }
 
     int[][] matrix = new int[][]{
@@ -40,21 +42,30 @@ public class Main {
             System.out.println();
         }
     }
-
-    void scanRows() {
-        int count = 0;
-        String regex= "^.*22222.*$";
-        List<int[]> match = new ArrayList<>();
-        Pattern p= Pattern.compile(regex);
-            for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix[i].length; j++) {
-                    match.add(matrix[i]);
-                    Matcher m= p.matcher((ArrayList<int[]>) match);
-
-                }
+    public int isWinner(){
+        int i,j;
+        for(i=0;i< matrix.length;i++){
+            for(j=0;j< matrix[i].length;j++){
+                if(j<=10 && matrix[i][j]*matrix[i][j+1]*matrix[i][j+2]*matrix[i][j+3]*matrix[i][j+4]==1)
+                    return 1;
+                if(j<=10 && matrix[i][j]*matrix[i][j+1]*matrix[i][j+2]*matrix[i][j+3]*matrix[i][j+4]==32)
+                    return 2;
+                if(i<=10 && matrix[i][j]*matrix[i+1][j]*matrix[i+2][j]*matrix[i+3][j]*matrix[i+4][j]==1)
+                    return 1;
+                if(i<=10 && matrix[i][j]*matrix[i+1][j]*matrix[i+2][j]*matrix[i+3][j]*matrix[i+4][j]==32)
+                    return 2;
+                if(j<=10 && i<=10 && matrix[i][j]*matrix[i+1][j+1]*matrix[i+2][j+2]*matrix[i+3][j+3]*matrix[i+4][j+4]==1)
+                    return 1;
+                if(j<=10 && i<=10 && matrix[i][j]*matrix[i+1][j+1]*matrix[i+2][j+2]*matrix[i+3][j+3]*matrix[i+4][j+4]==32)
+                    return 2;
+                if(j>=4 && i<=10 && matrix[i][j]*matrix[i+1][j-1]*matrix[i+2][j-2]*matrix[i+3][j-3]*matrix[i+4][j-4]==1)
+                    return 1;
+                if(j>=4 && i<=10 && matrix[i][j]*matrix[i+1][j-1]*matrix[i+2][j-2]*matrix[i+3][j-3]*matrix[i+4][j-4]==32)
+                    return 2;
             }
-            System.out.println(match);
-            System.out.println(count);
         }
+        return 0;
+    }
+
     }
 
